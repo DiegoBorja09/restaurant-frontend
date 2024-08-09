eaaebda22539d9450a1e96ca07e644682e1c951d
Diego Borja <diegoborja09@gmail.com>
2024-08-08 20:22:35 -0500

    cambios

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrlInventory: 'http://kitchen-service:3002',
-    apiUrlOrder: 'http://order-service:3001',
-    apiUrlKitchen: 'http://inventory-service:3003',
-};
